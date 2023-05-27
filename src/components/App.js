cddc91a1fd98a5a027fe71e9e48ac0fdff8c7a42
I-Viktor <Lafasolka@gmail.com>
2023-05-27 10:02:28 +0300

    replaced switch on routes in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ShopsPage from './ShopsPage';
 import ShoppingCartPage from './ShoppingCartPage';
 
@@ -22,21 +22,21 @@ function App() {
 
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
+      <Routes>
+        <Route path="/" element={
           <ShopsPage
             selectedShop={selectedShop}
             handleShopSelection={handleShopSelection}
             handleAddToCart={handleAddToCart}
           />
-        </Route>
-        <Route path="/cart">
+        } />
+        <Route path="/cart" element={
           <ShoppingCartPage
             cartItems={cartItems}
             handleRemoveFromCart={handleRemoveFromCart}
           />
-        </Route>
-      </Switch>
+        } />
+      </Routes>
     </Router>
   );
 }
